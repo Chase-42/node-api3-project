d12b8a74264408e7e3929ca2b90cff419460f7a6
Chase-42 <chaseofthecollins@gmail.com>
2020-02-19 19:48:52 -0700

    stretch

--- routers/welcome.js
+++ routers/welcome.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to the server!");
+  res.json({
+    message: process.env.WELCOME_MESSAGE || "Welcome to the server!"
+  });
 });
 
 module.exports = router;
